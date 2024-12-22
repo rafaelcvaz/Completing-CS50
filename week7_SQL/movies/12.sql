6d41c334e19713e25fff5bc59bcefec117e6eb77
rafaelcvaz <rafaeldecamargovaz@outlook.com>
2024-12-22 13:39:58 -0300

    uploading more exercises from week 7

---
+++ week7_SQL/movies/12.sql
@@ -0,0 +1,4 @@
+-- List the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
+SELECT title FROM movies WHERE id IN
+   (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Bradley Cooper') AND movie_id IN
+      (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = 'Jennifer Lawrence')));
